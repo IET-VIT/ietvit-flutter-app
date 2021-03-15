1a480d151748a973f3e8c72882b0e720cce41304
dakshp07 <dpaleria@gmail.com>
2021-03-15 22:27:44 +0530

    Done with Login and Register Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,218 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:ietvit_app/register%20page.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(brightness: Brightness.light),
+      home: new HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        backgroundColor: Colors.white,
+        body: new SingleChildScrollView(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              new Padding(
+                padding: const EdgeInsets.only(top: 50),
+              ),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  new Image.asset(
+                    "assets/images/iet_logo.png",
+                    height: 150,
+                    width: 200,
+                  ),
+                  new Image.asset(
+                    "assets/images/hackoff_logo.png",
+                    height: 150,
+                    width: 200,
+                  )
+                ],
+              ),
+              new Padding(
+                padding: const EdgeInsets.only(top: 100),
+              ),
+              new Container(
+                height: 595,
+                width: 500,
+                decoration: new BoxDecoration(
+                    color: Color(0xFF57B7DF),
+                    borderRadius: new BorderRadius.only(
+                        topRight: Radius.circular(50),
+                        topLeft: Radius.circular(50))),
+                child: new Column(
+                  children: [
+                    new Padding(padding: const EdgeInsets.all(10)),
+                    new Center(
+                      child: new Text(
+                        "Login",
+                        style: TextStyle(
+                            fontSize: 45,
+                            color: Color(0xFF0B2751),
+                            fontFamily: "poppins_semibold"),
+                      ),
+                    ),
+                    new Padding(padding: const EdgeInsets.only(top: 25)),
+                    new Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        new Container(
+                          height: 60,
+                          width: 300,
+                          decoration: new BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(40))),
+                          child: new Card(
+                            shape: new RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(40))),
+                            color: Colors.white,
+                            child: new Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                new SvgPicture.asset(
+                                  "assets/icons/email.svg",
+                                  height: 40,
+                                  width: 30,
+                                ),
+                                new Text("Login with E-mail",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        color: Color(0xFF0B2751),
+                                        fontFamily: "poppins_semibold"))
+                              ],
+                            ),
+                          ),
+                        ),
+                        new Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                        ),
+                        new Container(
+                          height: 60,
+                          width: 300,
+                          decoration: new BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(40))),
+                          child: new Card(
+                            shape: new RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(40))),
+                            color: Colors.white,
+                            child: new Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                new SvgPicture.asset(
+                                  "assets/icons/pichai.svg",
+                                  height: 40,
+                                  width: 30,
+                                ),
+                                new Text("Login with Google",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        color: Color(0xFF0B2751),
+                                        fontFamily: "poppins_semibold"))
+                              ],
+                            ),
+                          ),
+                        ),
+                        new Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                        ),
+                        new Container(
+                          height: 60,
+                          width: 300,
+                          decoration: new BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(40))),
+                          child: new Card(
+                            shape: new RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(40))),
+                            color: Colors.white,
+                            child: new Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                new SvgPicture.asset(
+                                  "assets/icons/github.svg",
+                                  height: 40,
+                                  width: 30,
+                                ),
+                                new Text("Login with GitHub",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        color: Color(0xFF0B2751),
+                                        fontFamily: "poppins_semibold"))
+                              ],
+                            ),
+                          ),
+                        ),
+                        new Padding(
+                          padding: const EdgeInsets.only(top: 140),
+                        ),
+                        new Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            new GestureDetector(
+                              child: new Container(
+                                width: 140,
+                                height: 40,
+                                child: new Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    new Text(
+                                      "REGISTER",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.white,
+                                          fontFamily: "poppins_semibold"),
+                                    ),
+                                  ],
+                                ),
+                                decoration: new BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(50)),
+                                    gradient: LinearGradient(colors: [
+                                      Color(0xFF0B2751),
+                                      Color(0xFFAAD6EA)
+                                    ])),
+                              ),
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (BuildContext context) =>
+                                            RegPage()));
+                              },
+                            )
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
---
+++ lib/register page.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class RegPage extends StatefulWidget {
+  @override
+  _RegPageState createState() => _RegPageState();
+}
+
+class _RegPageState extends State<RegPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: new SingleChildScrollView(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              new Padding(
+                padding: const EdgeInsets.only(top: 50),
+              ),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  new Image.asset(
+                    "assets/images/iet_logo.png",
+                    height: 150,
+                    width: 200,
+                  ),
+                  new Image.asset(
+                    "assets/images/hackoff_logo.png",
+                    height: 150,
+                    width: 200,
+                  )
+                ],
+              ),
+              new Padding(
+                padding: const EdgeInsets.only(top: 100),
+              ),
+              new Container(
+                height: 595,
+                width: 500,
+                decoration: new BoxDecoration(
+                    color: Color(0xFF57B7DF),
+                    borderRadius: new BorderRadius.only(
+                        topRight: Radius.circular(50),
+                        topLeft: Radius.circular(50))),
+                child: new Column(
+                  children: [
+                    new Padding(padding: const EdgeInsets.all(10)),
+                    new Center(
+                      child: new Text(
+                        "Register with E-Mail",
+                        style: TextStyle(
+                            fontSize: 30,
+                            color: Color(0xFF0B2751),
+                            fontFamily: "poppins_semibold"),
+                      ),
+                    ),
+                    new Padding(padding: const EdgeInsets.only(top: 25)),
+                    new Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        new Container(
+                            height: 60,
+                            width: 350,
+                            decoration: new BoxDecoration(
+                                color: Colors.white,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(40))),
+                            child: new TextField(
+                              decoration: new InputDecoration(
+                                  border: InputBorder.none,
+                                  focusedBorder: InputBorder.none,
+                                  enabledBorder: InputBorder.none,
+                                  contentPadding: const EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 15),
+                                  hintText: "E-Mail",
+                                  hintStyle: TextStyle(
+                                      fontSize: 20,
+                                      fontFamily: "poppins_semibold")),
+                            )),
+                        new Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                        ),
+                        new Container(
+                            height: 60,
+                            width: 350,
+                            decoration: new BoxDecoration(
+                                color: Colors.white,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(40))),
+                            child: new TextField(
+                              decoration: new InputDecoration(
+                                  border: InputBorder.none,
+                                  focusedBorder: InputBorder.none,
+                                  enabledBorder: InputBorder.none,
+                                  contentPadding: const EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 15),
+                                  hintText: "Password",
+                                  hintStyle: TextStyle(
+                                      fontSize: 20,
+                                      fontFamily: "poppins_semibold")),
+                            )),
+                        new Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                        ),
+                        new GestureDetector(
+                          child: new Container(
+                              height: 60,
+                              width: 350,
+                              decoration: new BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(40))),
+                              child: new TextField(
+                                decoration: new InputDecoration(
+                                    border: InputBorder.none,
+                                    focusedBorder: InputBorder.none,
+                                    enabledBorder: InputBorder.none,
+                                    contentPadding: const EdgeInsets.symmetric(
+                                        horizontal: 10, vertical: 15),
+                                    hintText: "Confirm Password",
+                                    hintStyle: TextStyle(
+                                        fontSize: 20,
+                                        fontFamily: "poppins_semibold")),
+                              )),
+                        ),
+                        new Padding(
+                          padding: const EdgeInsets.only(top: 110),
+                        ),
+                        new GestureDetector(
+                          child: new Container(
+                            width: 350,
+                            height: 60,
+                            child: new Row(
+                              children: [
+                                new Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 40, horizontal: 50),
+                                ),
+                                new Text(
+                                  "REGISTER",
+                                  style: TextStyle(
+                                      fontSize: 30,
+                                      color: Colors.white,
+                                      fontFamily: "poppins_semibold"),
+                                ),
+                              ],
+                            ),
+                            decoration: new BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(50)),
+                                gradient: LinearGradient(colors: [
+                                  Color(0xFF0B2751),
+                                  Color(0xFFAAD6EA)
+                                ])),
+                          ),
+                          onTap: () {},
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
