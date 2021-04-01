17493fec0a9e7445e6a56fa1eb600aa6bad4f779
dakshp07 <dpaleria@gmail.com>
2021-04-01 10:32:32 +0530

    Added ImagePicker()

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,42 +1,58 @@
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
-
-
 class profile extends StatefulWidget {
   @override
   _profileState createState() => _profileState();
 }
 
-
 class _profileState extends State<profile> {
   final User user = FirebaseAuth.instance.currentUser;
+  File _image;
+  bool isImageThere = false;
+  final picker = ImagePicker();
 
-  createAlertDialog(BuildContext context, String qr_data){
-    return showDialog(context: context, builder: (context){
-      return AlertDialog(
-        title: Text(
-          "Your QR Code",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-              fontSize: 22, color: Color(0xFF0B2751), fontFamily: "acme"),
-        ),
-        content: new Container(
-          alignment: Alignment.center,
-          width: 180,
-          height: 180,
-          child:
-          new QrImage(
-            data: qr_data,
-            size: 180,
-            version: QrVersions.auto,
-          ),
-        ),
-      );
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+        isImageThere = true;
+      } else {
+        print('No image selected.');
+      }
     });
+  }
+
+  createAlertDialog(BuildContext context, String qr_data) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(
+              "Your QR Code",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 22, color: Color(0xFF0B2751), fontFamily: "acme"),
+            ),
+            content: new Container(
+              alignment: Alignment.center,
+              width: 180,
+              height: 180,
+              child: new QrImage(
+                data: qr_data,
+                size: 180,
+                version: QrVersions.auto,
+              ),
+            ),
+          );
+        });
   }
 
   @override
@@ -58,7 +74,6 @@ class _profileState extends State<profile> {
               fontSize: 20, color: Colors.white, fontFamily: "roboto_medium"),
         ),
       ),
-
       floatingActionButton: new Container(
         width: 56,
         height: 56,
@@ -66,7 +81,7 @@ class _profileState extends State<profile> {
           child: new FloatingActionButton(
             backgroundColor: Color(0xFF28B9E4),
             onPressed: () {
-            createAlertDialog(context, user.email);
+              createAlertDialog(context, user.email);
             },
             child: new Padding(
               padding: const EdgeInsets.all(15),
@@ -75,7 +90,6 @@ class _profileState extends State<profile> {
           ),
         ),
       ),
-
       body: new SingleChildScrollView(
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -95,22 +109,29 @@ class _profileState extends State<profile> {
                           padding: const EdgeInsets.all(8.0),
                           child: Container(
                             child: new CircleAvatar(
-                              backgroundImage:
-                                  AssetImage("assets/images/iet_logo.png"),
+                              backgroundImage: isImageThere
+                                  ? FileImage(_image)
+                                  : AssetImage("assets/images/iet_logo.png"),
                               radius: 70.0,
                             ),
                           ),
                         ),
                       )),
                     ),
-                    new Padding(
-                        padding: const EdgeInsets.only(
-                           left: 220,bottom: 10),
-                        child: new SvgPicture.asset(
-                          "assets/icons/ic_chats_nav.svg",
-                          height: 32,
-                          width: 32,
-                        )),
+                    new GestureDetector(
+                      child: new Padding(
+                          padding: const EdgeInsets.only(left: 240, top: 10),
+                          child: new CircleAvatar(
+                            backgroundColor: Color(0xFF28B9E4),
+                            radius: 20,
+                            child: new Icon(
+                              Icons.edit,
+                              size: 30,
+                              color: Colors.grey[600],
+                            ),
+                          )),
+                      onTap: getImage,
+                    ),
                   ],
                 ),
                 new Padding(
@@ -132,18 +153,17 @@ class _profileState extends State<profile> {
                   width: queryData.size.width,
                 ),
                 new Container(
-                  height: queryData.size.height * 0.65,
+                  height: queryData.size.height * 0.53,
                   width: queryData.size.width,
                   decoration: new BoxDecoration(
                     color: Color(0xFF28B9E4),
                   ),
-
                   child: new Column(
-                    // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      new Padding(padding: const EdgeInsets.only(top :20),),
+                      new Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                      ),
                       new Row(
-
                         children: [
                           new Padding(
                             padding: const EdgeInsets.only(left: 64),
@@ -157,14 +177,18 @@ class _profileState extends State<profile> {
                             padding: const EdgeInsets.only(left: 64),
                           ),
                           //new Padding(padding: const EdgeInsets.all(40)),
-                          new Text("testing@test.com",
+                          new Text(
+                            "testing@test.com",
                             style: TextStyle(
-                                fontSize: 20, color: Colors.white, fontFamily: "acme"),),
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontFamily: "acme"),
+                          ),
                         ],
                       ),
-
-                      new Padding(padding: const EdgeInsets.only(top :20),),
-
+                      new Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                      ),
                       new Row(
                         children: [
                           new Padding(
@@ -179,13 +203,18 @@ class _profileState extends State<profile> {
                             padding: const EdgeInsets.only(left: 64),
                           ),
                           //new Padding(padding: const EdgeInsets.all(40)),
-                          new Text("9999900000",style: TextStyle(
-                              fontSize: 20, color: Colors.white, fontFamily: "acme"),),
+                          new Text(
+                            "9999900000",
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontFamily: "acme"),
+                          ),
                         ],
                       ),
-
-                      new Padding(padding: const EdgeInsets.only(top :20),),
-
+                      new Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                      ),
                       new Row(
                         children: [
                           new Padding(
@@ -200,13 +229,18 @@ class _profileState extends State<profile> {
                             padding: const EdgeInsets.only(left: 64),
                           ),
                           //  new Padding(padding: const EdgeInsets.all(40)),
-                          new Text("Board",style: TextStyle(
-                              fontSize: 20, color: Colors.white, fontFamily: "acme"),),
+                          new Text(
+                            "Board",
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontFamily: "acme"),
+                          ),
                         ],
                       ),
-
-                      new Padding(padding: const EdgeInsets.only(top :20),),
-
+                      new Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                      ),
                       new Row(
                         children: [
                           new Padding(
@@ -221,13 +255,18 @@ class _profileState extends State<profile> {
                             padding: const EdgeInsets.only(left: 64),
                           ),
                           // new Padding(padding: const EdgeInsets.all(40)),
-                          new Text("Secondary Role",style: TextStyle(
-                              fontSize: 20, color: Colors.white, fontFamily: "acme"),),
+                          new Text(
+                            "Secondary Role",
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontFamily: "acme"),
+                          ),
                         ],
                       ),
-
-                      new Padding(padding: const EdgeInsets.only(top :20),),
-
+                      new Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                      ),
                       new Row(
                         children: [
                           new Padding(
@@ -242,8 +281,13 @@ class _profileState extends State<profile> {
                           new Padding(
                             padding: const EdgeInsets.only(left: 64),
                           ),
-                          new Text("Timetable",style: TextStyle(
-                              fontSize: 20, color: Colors.white, fontFamily: "acme"),),
+                          new Text(
+                            "Timetable",
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontFamily: "acme"),
+                          ),
                         ],
                       ),
                     ],
